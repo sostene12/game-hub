9509b05b821a9383e46724d04ee491244f968204
sostene12 <ngarukiyimanasostene@gmail.com>
2024-03-07 16:05:31 +0200

    show loading skeletons

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,9 +1,11 @@
 import { SimpleGrid, Text } from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
+import GameCardSkeleton from "./GameCardSkeleton";
 
 const GameGrid = () => {
-  const { games, error } = useGames();
+  const { games, error,isLoading } = useGames();
+  const skeletos = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
 
   if (error) return <div>{error}</div>;
   console.log(games);
@@ -12,6 +14,7 @@ const GameGrid = () => {
     <>
       {error && <Text>{error}</Text>}
       <SimpleGrid columns={{sm:1,md:2,lg:3,xl:5}} spacing={6} padding="10px">
+        {isLoading && skeletos.map((skeleton) => <GameCardSkeleton key={skeleton} />)}
         {games?.map((game) => (
             <GameCard key={game.id} game={game} />
         ))}
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -13,7 +13,7 @@ export interface Game {
   name: string;
   background_image: string;
   parent_platforms: { platform: Platform }[];
-  metacritic:number;
+  metacritic: number;
 }
 
 interface FetchGamesResponse {
@@ -24,22 +24,27 @@ interface FetchGamesResponse {
 const useGames = () => {
   const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const controller = new AbortController();
-
+    setIsLoading(true);
     apiClient
       .get<FetchGamesResponse>("/games", { signal: controller.signal })
-      .then((res) => setGames(res.data.results))
+      .then((res) => {
+        setGames(res.data.results);
+        setIsLoading(false);
+      })
       .catch((error) => {
         if (error instanceof CanceledError) return;
         setError(error.message);
+        setIsLoading(false);
       });
 
     return () => controller.abort();
   }, []);
 
-  return { games, error };
+  return { games, error, isLoading };
 };
 
 export default useGames;
